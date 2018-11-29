b290ea6e5d4e63266f43bf3f5877346395da95b6
bertooon <filipow.bartosz@gmail.com>
2018-11-29 13:27:51 +0100

    przebiegi z czujników

---
+++ przebiegi.m
@@ -0,0 +1,18 @@
+pomiar = csvread('gyro2(8,16ms).csv',0,0);
+dt = 0.00816;
+t = pomiar(:,1)*dt;
+ch1 = pomiar(:,2)*(2/(2^12)); % acc x (g)
+ch2 = pomiar(:,3)*(2/(2^12)); % acc y 
+ch3 = pomiar(:,4)*(2/(2^12)); % acc z
+ch4 = (pomiar(:,5))*(2500/(2^12)); % gyro x ( deg/s)
+ch5 = (pomiar(:,6))*(2500/(2^12)); % gyro y
+ch6 = (pomiar(:,7))*(2500/(2^12)); % gyro z
+calka1 = zeros(length(ch1),1);
+calka2 = zeros(length(ch1),1);
+calka3 = zeros(length(ch1),1);
+for i=2:length(ch1)
+    calka1(i) = calka1(i-1) + ch4(i)*dt;
+    calka2(i) = calka2(i-1) + ch5(i)*dt;
+    calka3(i) = calka3(i-1) + ch6(i)*dt;
+end
+plot(t,calka1,t,calka2,t,calka3); %przymitywnie scalkowane odczyty z zyroskopu powinny dac wychylenie, podczas zdejmowania pomiarow wychylalem obiekt o +/- 45 stopni w rozne strony
