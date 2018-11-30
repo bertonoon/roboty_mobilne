99dda2b4a63ccceb51d622d3e6fbb45815e428af
waciarz <45456097+waciarz@users.noreply.github.com>
2018-11-30 20:02:03 +0100

    Update przebiegi.m

--- przebiegi.m
+++ przebiegi.m
@@ -1,12 +1,12 @@
-pomiar = csvread('gyro2(8,16ms).csv',0,0);
+pomiar = csvread('pomiary.csv',0,0);
 dt = 0.00816;
 t = pomiar(:,1)*dt;
 ch1 = pomiar(:,2)*(2/(2^12)); % acc x (g)
 ch2 = pomiar(:,3)*(2/(2^12)); % acc y 
 ch3 = pomiar(:,4)*(2/(2^12)); % acc z
-ch4 = (pomiar(:,5))*(2500/(2^12)); % gyro x ( deg/s)
-ch5 = (pomiar(:,6))*(2500/(2^12)); % gyro y
-ch6 = (pomiar(:,7))*(2500/(2^12)); % gyro z
+ch4 = (pomiar(:,5))*(250/(2^12)); % gyro x ( deg/s)
+ch5 = (pomiar(:,6))*(250/(2^12)); % gyro y
+ch6 = (pomiar(:,7))*(250/(2^12)); % gyro z
 calka1 = zeros(length(ch1),1);
 calka2 = zeros(length(ch1),1);
 calka3 = zeros(length(ch1),1);
